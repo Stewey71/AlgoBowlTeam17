0e9d01a58551a7af4de4acf5f182bea10f52c00c
Stewey71 <zane.deaton@yahoo.com>
2019-09-29 18:29:48 -0600

    Added readInput Functions and sample input.txt

--- Heuristic.py
+++ Heuristic.py
@@ -1,1 +1,13 @@
-
+def readInput(filename):
+    outputTup = []
+    uniSet = {}
+    f = open(filename, "r")
+    uniSetNum = int(f.readline())
+    setNums = f.readline()
+    for line in f:
+        tempSet = {int(j) for j in line.split()}
+        tempWeight = int(f.readline());
+        outputTup.append((tempSet,tempWeight))
+    uniSet = {i for i in range(1,uniSetNum+1)}
+    return uniSet, outputTup
+print(readInput("input.txt"))
