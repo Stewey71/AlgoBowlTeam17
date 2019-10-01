64713d321c82100865d19c165a5dfdea9c11e4b3
zsmeton <zsmeton@mines.edu>
2019-10-01 12:59:01 -0600

    Implemented spotlight search algorithm

--- Heuristic.py
+++ Heuristic.py
@@ -76,20 +76,92 @@ def k_min(list, k, key=None) -> list:
     smallest = [1e9 for _ in range(k)]
     for elem in list:
         for i, val in enumerate(smallest):
-            try:
-                if key(elem) < key(val):
+            if key is not None:
+                try:
+                    if key(elem) < key(val):
+                        smallest.insert(i, elem)
+                        smallest = smallest[:k]
+                        break
+                except (TypeError, AttributeError):
                     smallest.insert(i, elem)
                     smallest = smallest[:k]
                     break
-            except TypeError:
+            elif elem < val:
                 smallest.insert(i, elem)
                 smallest = smallest[:k]
                 break
+
     return smallest
 
 
-# TODO: Implement Spotlight search
-# TODO: Implement spotlight search using annealing for choosing the k paths
+class SpotlightSearch:
+    class Path:
+        def __init__(self, current_set, chosen_sets, cost, options):
+            self.current_set = current_set
+            self.chosen_sets = chosen_sets
+            self.cost = cost
+            self.options = options
+
+        def add_to_current_set(self, addition: set):
+            self.current_set |= addition
+            self.current_set = set(self.current_set)
+
+        def add_to_chosen_sets(self, addition: int):
+            self.chosen_sets.append(addition)
+
+        def add_to_cost(self, addition: int):
+            self.cost += addition
+
+        def remove_from_options(self, subset: set, weight: int):
+            while self.options.count((subset, weight)) > 0:
+                self.options.remove((subset, weight))
+
+        def copy(self):
+            return SpotlightSearch.Path(self.current_set.copy(), self.chosen_sets.copy(), self.cost,
+                                        self.options.copy())
+
+        def get_reweighted(self):
+            return self.cost / len(self.current_set)
+
+        def __repr__(self):
+            return f'Sets: {sorted(self.chosen_sets)}, Cost: {self.cost}'
+
+    def __init__(self, universal_set, subsets, chosing_method, *args, **kwargs):
+        self.paths = []
+        self.universal_set = universal_set
+        self.subsets = subsets
+        self.subsets_master = dict([(f"{key[0]}{key[1]}", i + 1) for i, key in enumerate(subsets)])
+        self.chosing_method = chosing_method
+        self.chosing_method_args = args
+        self.chosing_method_kwargs = kwargs
+
+    def _set_to_int(self, sety_boi: set, weight: int):
+        return self.subsets_master[f"{sety_boi}{weight}"]
+
+    def solve(self):
+        # Seed solution with all options
+        tempPaths = []
+        for subset, cost in self.subsets:
+            path = SpotlightSearch.Path(subset, [self._set_to_int(subset, cost)], cost, self.subsets.copy())
+            path.remove_from_options(subset, cost)
+            tempPaths.append(path)
+
+        while not any([not self.universal_set - temp.current_set for temp in self.paths]):
+            k_best_options = self.chosing_method(tempPaths, *self.chosing_method_args, **self.chosing_method_kwargs)
+            self.paths.clear()
+            self.paths.extend(k_best_options)
+            tempPaths = []
+            # Calculate weights for each path
+            for path in self.paths:
+                for subset, cost in path.options:
+                    tempPath = path.copy()
+                    tempPath.add_to_current_set(subset)
+                    tempPath.add_to_chosen_sets(self._set_to_int(subset, cost))
+                    tempPath.remove_from_options(subset, cost)
+                    tempPath.add_to_cost(cost)
+                    tempPaths.append(tempPath)
+        return [path for path in self.paths if not self.universal_set - path.current_set]
+
 
 def solve_hill_climbing(universal_set: set, subsets: list):
     """
@@ -114,12 +186,15 @@ def solve_hill_climbing(universal_set: set, subsets: list):
         # Update costs, current set, chosen set, and subsets
         cost += best_option[1]
         current_set |= best_option[0]
-        subsets.remove(best_option[:2])
+        while subsets.count(best_option[:2]):
+            subsets.remove(best_option[:2])
         chosen_sets.append(subset_master.get(f"{best_option[0]}{best_option[1]}"))
     return current_set, cost, sorted(chosen_sets)
 
 
 if __name__ == "__main__":
-    x, y = readInput("testInputs/test1.txt")
+    x, y = readInput("testInputs/test3.txt")
     print(solve_hill_climbing(x.copy(), y.copy())[1:])
-    print(solve_spotlight_search(x.copy(), y.copy()))
+    # TODO: Implement spotlight search using annealing for choosing the k paths
+    ss = SpotlightSearch(x.copy(), y.copy(), chosing_method=k_min, k=30, key=operator.methodcaller('get_reweighted'))
+    print(k_min(ss.solve(), 1, key=operator.attrgetter('cost')))
